8e699b8eb298f1e132a97487e0fa9f6fba4668d8
Akane <710105188@qq.com>
2025-10-23 20:50:51 +0800

    feat(utils): Add Ediff configuration for improved diffing
    Configure Ediff with custom window and split settings for better
    usability

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -34,5 +34,21 @@
     (grep-apply-setting
      'grep-find-template "rg --color=auto --null -nH --no-heading -e <R> <D>")))
 
+;;----------------------------------------------------------------------------
+;; Ediff Configuration
+;;----------------------------------------------------------------------------
+
+;; A comprehensive visual interface to diff & patch
+(use-package ediff
+  :ensure nil
+  :hook(;; show org ediffs unfolded
+        (ediff-prepare-buffer . outline-show-all)
+        ;; restore window layout when done
+        (ediff-quit . winner-undo))
+  :config
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain
+        ediff-split-window-function 'split-window-horizontally
+        ediff-merge-split-window-function 'split-window-horizontally))
+
 (provide 'init-utils)
 ;;; init-utils.el ends here
