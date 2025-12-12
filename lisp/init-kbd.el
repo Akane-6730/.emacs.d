1f9acbf1c522a0e7a84c78a50b833626daf07fe8
Akane <710105188@qq.com>
2025-12-12 13:59:03 +0800

    feat(kbd): Add keybindings for buffer and global text scaling
    Introduce shortcuts for both buffer-local and global font size
    adjustment using s-=, s-- for buffer, and C-+, C-_ for global
    scaling.

--- lisp/init-kbd.el
+++ lisp/init-kbd.el
@@ -13,9 +13,38 @@
 (global-set-key (kbd "s-a") #'mark-whole-buffer)
 (global-set-key (kbd "s-c") #'kill-ring-save)
 
-;;----------------------------------------------------------------------------
+;;---------------------------------------------------------------------
+;; Text Scaling
+;;---------------------------------------------------------------------
+
+;; Buffer Text Scaling
+(global-set-key (kbd "s-=") #'text-scale-increase)
+(global-set-key (kbd "s--") #'text-scale-decrease)
+
+;; Global Text Scaling
+(defun my/global-text-scale-adjust (inc)
+  "Adjust the global font size by INC factor."
+  (let* ((old-face-attribute (face-attribute 'default :height))
+         (new-face-attribute (+ old-face-attribute inc)))
+    (set-face-attribute 'default nil :height new-face-attribute)
+    (message "Global font size set to %d" new-face-attribute)))
+
+(defun my/global-text-scale-increase ()
+  "Increase global font size."
+  (interactive)
+  (my/global-text-scale-adjust 10))
+
+(defun my/global-text-scale-decrease ()
+  "Decrease global font size."
+  (interactive)
+  (my/global-text-scale-adjust -10))
+
+(global-set-key (kbd "C-+") #'my/global-text-scale-increase)
+(global-set-key (kbd "C-_") #'my/global-text-scale-decrease)
+
+;;---------------------------------------------------------------------
 ;; Kitty Keyboard Protocol (TUI only)
-;;----------------------------------------------------------------------------
+;;---------------------------------------------------------------------
 
 ;; Enable extended key combinations in TUI
 (use-package kkp
