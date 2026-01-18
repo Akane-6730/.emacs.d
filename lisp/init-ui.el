7d4cf6ac2280485db2244653023240a2f86cef3d
Akane <710105188@qq.com>
2026-01-18 16:38:28 +0800

    feat(ui): Add posframe and vertico-posframe support
    - Configure posframe with custom border and fringe styling
    - Add custom poshandler for bottom-center positioning
    - Enable vertico-posframe integration for a centered completion UI

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -262,5 +262,37 @@
   :config (dolist (mode '(dashboard-mode emacs-news-mode))
             (add-to-list 'page-break-lines-modes mode)))
 
+(use-package posframe
+  :hook (after-load-theme . posframe-delete-all)
+  :init
+  (defface posframe-border
+    `((t (:inherit region)))
+    "Face used by the `posframe' border."
+    :group 'posframe)
+  (defvar posframe-border-width 2
+    "Default posframe border width.")
+  :config
+  (with-no-warnings
+    (defun my-posframe--prettify-frame (&rest _)
+      (set-face-background 'fringe nil posframe--frame))
+    (advice-add #'posframe--create-posframe :after #'my-posframe--prettify-frame)
+
+    (defun posframe-poshandler-frame-center-near-bottom (info)
+      (cons (/ (- (plist-get info :parent-frame-width)
+                  (plist-get info :posframe-width))
+               2)
+            (/ (+ (plist-get info :parent-frame-height)
+                  (* 2 (plist-get info :font-height)))
+               2)))))
+
+(use-package vertico-posframe
+    :functions posframe-poshandler-frame-center-near-bottom
+    :hook (vertico-mode . vertico-posframe-mode)
+    :init (setq vertico-posframe-poshandler
+                #'posframe-poshandler-frame-center-near-bottom
+                vertico-posframe-parameters
+                '((left-fringe  . 8)
+                  (right-fringe . 8))))
+
 (provide 'init-ui)
 ;;; init-ui.el ends here
