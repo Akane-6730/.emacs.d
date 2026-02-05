324ab0793fc1afe07cf85b5db9677792dac63d61
Akane <710105188@qq.com>
2026-02-05 20:40:06 +0800

    feat(workspace): Improve buffer sorting and exclusions
    - Add `*Ibuffer*` to the tabspaces exclusion list
    - Move `*scratch*` and `*Messages*` buffers to the end of the workspace list

--- lisp/init-workspace.el
+++ lisp/init-workspace.el
@@ -19,31 +19,35 @@
   (tabspaces-default-tab "Default")
   (tabspaces-remove-to-default t)
   (tabspaces-include-buffers '("*scratch*" "*Messages*"))
-  (tabspaces-exclude-buffers '("*eat*" "*vterm*" "*shell*" "*eshell*" "*dashboard*"))
+  (tabspaces-exclude-buffers '("*eat*" "*vterm*" "*shell*" "*eshell*" "*dashboard*" "*Ibuffer*"))
   (tab-bar-new-tab-choice "*scratch*")
   ;; sessions - disabled for faster startup/shutdown
   (tabspaces-session nil)
   (tabspaces-session-auto-restore nil)
   :config
-  (with-no-warnings
-    ;; Filter Buffers for Consult-Buffer
-    (with-eval-after-load 'consult
-      ;; hide full buffer list (still available with "b" prefix)
-      (consult-customize consult-source-buffer :hidden t :default nil)
-      ;; set consult-workspace buffer list
-      (defvar consult-source-workspace
-        (list :name     "Workspace Buffer"
-              :narrow   ?w
-              :history  'buffer-name-history
-              :category 'buffer
-              :state    #'consult--buffer-state
-              :default  t
-              :items    (lambda () (consult--buffer-query
-                                    :predicate #'tabspaces--local-buffer-p
-                                    :sort 'visibility
-                                    :as #'buffer-name)))
-        "Set workspace buffer list for consult-buffer.")
-      (add-to-list 'consult-buffer-sources 'consult-source-workspace))))
+  ;; Filter Buffers for Consult-Buffer
+  (with-eval-after-load 'consult
+    ;; hide full buffer list (still available with "b" prefix)
+    (consult-customize consult-source-buffer :hidden t :default nil)
+    ;; set consult-workspace buffer list
+    (defvar consult--source-workspace
+      (list :name     "Workspace Buffers"
+            :narrow   ?w
+            :history  'buffer-name-history
+            :category 'buffer
+            :state    #'consult--buffer-state
+            :default  t
+            :items    (lambda ()
+                        (let* ((dominated '("*scratch*" "*Messages*"))
+                               (buffers (consult--buffer-query
+                                         :predicate #'tabspaces--local-buffer-p
+                                         :sort 'visibility
+                                         :as #'buffer-name)))
+                          ;; Move dominated buffers to the end
+                          (append (seq-remove (lambda (b) (member b dominated)) buffers)
+                                  (seq-filter (lambda (b) (member b dominated)) buffers)))))
+      "Workspace buffer list for consult-buffer.")
+    (add-to-list 'consult-buffer-sources 'consult--source-workspace)))
 
 (provide 'init-workspace)
 ;;; init-workspace.el ends here
