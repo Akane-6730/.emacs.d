9de7182395e9d4d791989a7e361d31456b71c36e
Akane <710105188@qq.com>
2026-02-05 16:36:08 +0800

    refactor(spell): Simplify flyspell and region skip logic
    - Replace manual key unbinding functions with `:bind`
    - Move ispell configuration variables to `:custom`
    - Introduce a helper for managing buffer-local skip regions
    - Consolidate shared LaTeX math skip patterns for Org and Markdown

--- lisp/init-spell.el
+++ lisp/init-spell.el
@@ -4,79 +4,56 @@
 ;;
 ;; Configures on-the-fly spell checking using the built-in `flyspell`.
 ;;
-;; This setup enables spell checking for human-readable text.
-;; In document modes (like Org or Markdown), it checks the whole buffer.
-;; In programming modes, it intelligently checks only comments and strings.
+;; In document modes (Org/Markdown), checks the whole buffer.
+;; In programming modes, checks only comments and strings.
 ;;
-;; To correct a misspelled word, move the cursor over it and use the default
-;; command `flyspell-auto-correct-word` (default bound to `C-M-i`).
-;;
-;; This feature requires the external program `aspell` to be installed.
+;; Requires external program `aspell` to be installed.
 
 ;;; Code:
-
-;;----------------------------------------------------------------------------
-;; Helper functions for Keybinding Management
-;;----------------------------------------------------------------------------
-
-(defun my-spell--unbind-all-conflicting-keys ()
-  "Globally unbind all potentially conflicting keys from flyspell.
-This creates a clean slate, and specific keys can be re-bound locally
-in the modes where they are desired."
-  (unbind-key "C-." flyspell-mode-map)
-  (unbind-key "C-;" flyspell-mode-map)
-  (unbind-key "C-," flyspell-mode-map))
-
-(defun my-spell--rebind-correction-keys-for-text ()
-  "Re-bind keys for non-programming modes.
-This function is hooked into text-centric modes to provide convenient
-correction keys, without affecting programming modes."
-  ;; We bind `C-,` locally for the buffer.
-  (define-key (current-local-map) (kbd "C-,") #'flyspell-goto-next-error))
-
-;;----------------------------------------------------------------------------
-;; On-the-fly Spell Checking
-;;----------------------------------------------------------------------------
 
 (use-package flyspell
   :ensure nil
   :diminish
   :if (executable-find "aspell")
+  :bind (:map flyspell-mode-map
+         ("C-;" . nil) ("C-," . nil) ("C-." . nil))
   :hook ((prog-mode . flyspell-prog-mode)
-         ;; This hook runs in ALL flyspell modes and removes all conflicting keys.
-         (flyspell-mode . my-spell--unbind-all-conflicting-keys)
-         (org-mode . org-skip-region-alist)
-         (markdown-mode . markdown-skip-region-alist))
+         (org-mode . flyspell-mode)
+         (markdown-mode . flyspell-mode))
+  :custom
+  (ispell-program-name "aspell")
+  (ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together"))
+  (flyspell-issue-message-flag nil)
   :config
-  ;; Don't spellcheck org blocks
-  (defun org-skip-region-alist ()
-    (make-local-variable 'ispell-skip-region-alist)
-    (dolist (pair '((org-property-drawer-re)
-                    ("~" "~") ("=" "=")
-                    ("^#\\+BEGIN_SRC" "^#\\+END_SRC")
-                    ("\\\\(" "\\\\)") ("\\[" "\\]")
-                    ("^\\\\begin{[^}]+}" "^\\\\end{[^}]+}")))
-      (add-to-list 'ispell-skip-region-alist pair)))
+  ;; Helper to setup skip regions
+  (defun my-spell--add-skip-regions (regions)
+    "Add REGIONS to `ispell-skip-region-alist' locally."
+    (setq-local ispell-skip-region-alist
+                (append regions ispell-skip-region-alist)))
 
-  (defun markdown-skip-region-alist ()
-    (make-local-variable 'ispell-skip-region-alist)
-    (dolist (pair '(("`" "`")
-                    ("^```" "^```")
-                    ("{{" "}}")
-                    ("\\\\(" "\\\\)") ("\\[" "\\]")
-                    ("^\\\\begin{[^}]+}" "^\\\\end{[^}]+}")))
-      (add-to-list 'ispell-skip-region-alist pair)))
-  ;; Use `aspell` as the backend and provide args for better suggestions.
-  (setq ispell-program-name "aspell"
-        ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together")
-        flyspell-issue-message-flag nil)  ; Suppress startup messages.
-  :init
-  ;; For each text-centric mode, enable `flyspell-mode` and then add back
-  ;; the specific keybindings we want for those modes.
-  (dolist (hook '(org-mode-hook
-                  markdown-mode-hook))
-    (add-hook hook #'flyspell-mode)
-    (add-hook hook #'my-spell--rebind-correction-keys-for-text)))
+  ;; Common LaTeX-style skip patterns
+  (defconst my-spell--latex-skip-regions
+    '(("\\\\(" "\\\\)") ("\\[" "\\]")
+      ("^\\\\begin{[^}]+}" "^\\\\end{[^}]+}"))
+    "Skip regions for LaTeX math environments.")
+
+  ;; Org-mode: skip properties, verbatim, code blocks, and LaTeX
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (my-spell--add-skip-regions
+               `((,org-property-drawer-re)
+                 ("~" "~") ("=" "=")
+                 ("^#\\+BEGIN_SRC" "^#\\+END_SRC")
+                 ,@my-spell--latex-skip-regions))))
+
+  ;; Markdown: skip inline code, fenced blocks, templates, and LaTeX
+  (add-hook 'markdown-mode-hook
+            (lambda ()
+              (my-spell--add-skip-regions
+               `(("`" "`")
+                 ("^```" "^```")
+                 ("{{" "}}")
+                 ,@my-spell--latex-skip-regions)))))
 
 (provide 'init-spell)
 ;;; init-spell.el ends here
