e5046f42dad453bad105807e9b5961ae1b217601
Akane <710105188@qq.com>
2026-02-10 14:17:27 +0800

    feat(spell): add buffer size limit for flyspell activation
    - Introduce `my-spell-buffer-size-limit` variable (512KB default) to skip spell-checking in large buffers
    - Replace direct mode hooks with conditional wrapper functions (`my-spell--flyspell-maybe` and `my-spell--flyspell-prog-maybe`)
    - Consolidate `org-mode` and `markdown-mode` into single hook entry for cleaner config

--- lisp/init-spell.el
+++ lisp/init-spell.el
@@ -17,14 +17,26 @@
   :if (executable-find "aspell")
   :bind (:map flyspell-mode-map
          ("C-;" . nil) ("C-," . nil) ("C-." . nil))
-  :hook ((prog-mode . flyspell-prog-mode)
-         (org-mode . flyspell-mode)
-         (markdown-mode . flyspell-mode))
+  :hook ((prog-mode . my-spell--flyspell-prog-maybe)
+         ((markdown-mode org-mode) . my-spell--flyspell-maybe))
   :custom
   (ispell-program-name "aspell")
   (ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together"))
   (flyspell-issue-message-flag nil)
   :config
+  (defvar my-spell-buffer-size-limit (* 512 1024)
+    "Skip flyspell in buffers larger than this (default 512KB).")
+
+  (defun my-spell--flyspell-maybe ()
+    "Enable `flyspell-mode' unless buffer is too large."
+    (unless (> (buffer-size) my-spell-buffer-size-limit)
+      (flyspell-mode 1)))
+
+  (defun my-spell--flyspell-prog-maybe ()
+    "Enable `flyspell-prog-mode' unless buffer is too large."
+    (unless (> (buffer-size) my-spell-buffer-size-limit)
+      (flyspell-prog-mode)))
+
   ;; Helper to setup skip regions
   (defun my-spell--add-skip-regions (regions)
     "Add REGIONS to `ispell-skip-region-alist' locally."
