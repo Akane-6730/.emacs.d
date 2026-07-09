f241aef8ef5825eb474ac481b9f2521a4e81f19f
Akane <710105188@qq.com>
2026-07-09 21:23:40 +0800

    refactor(prog): simplify hideshow config & improve comint scrolling

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -94,60 +94,7 @@
               ("C-S-<tab>" . hs-toggle-all)
               ("C-<iso-lefttab>" . hs-toggle-all)
               ("C-c C-h" . hs-hide-block)
-              ("C-c C-s" . hs-show-block))
-  :config
-  ;; More functions
-  ;; @see https://karthinks.com/software/simple-folding-with-hideshow/
-  (defun hs-cycle (&optional level)
-    (interactive "p")
-    (let (message-log-max
-          (inhibit-message t))
-      (if (= level 1)
-          (pcase last-command
-            ('hs-cycle
-             (hs-hide-level 1)
-             (setq this-command 'hs-cycle-children))
-            ('hs-cycle-children
-             (save-excursion (hs-show-block))
-             (setq this-command 'hs-cycle-subtree))
-            ('hs-cycle-subtree
-             (hs-hide-block))
-            (_
-             (if (not (hs-already-hidden-p))
-                 (hs-hide-block)
-               (hs-hide-level 1)
-               (setq this-command 'hs-cycle-children))))
-        (hs-hide-level level)
-        (setq this-command 'hs-hide-level))))
-
-  (defun hs-toggle-all ()
-    "Toggle hide/show all."
-    (interactive)
-    (pcase last-command
-      ('hs-toggle-all
-       (save-excursion (hs-show-all))
-       (setq this-command 'hs-global-show))
-      (_ (hs-hide-all))))
-
-  ;; Display line counts
-  (defun hs-display-code-line-counts (ov)
-    "Display line counts when hiding codes."
-    (when (eq 'code (overlay-get ov 'hs))
-      (overlay-put ov 'display
-                   (concat
-                    " "
-                    (propertize
-                     (if (char-displayable-p ?⏷) "⏷" "...")
-                     'face 'shadow)
-                    (propertize
-                     (format " (%d lines)"
-                             (count-lines (overlay-start ov)
-                                          (overlay-end ov)))
-                     'face '(:inherit shadow :height 0.8))
-                    " "))))
-
-  (setq hs-set-up-overlay #'hs-display-code-line-counts))
-
+              ("C-c C-s" . hs-show-block)))
 
 ;;----------------------------------------------------------------------------
 ;; Project-specific Settings
@@ -164,17 +111,18 @@
 ;; Visual Enhancements
 ;;----------------------------------------------------------------------------
 
-;; Prettify Symbols (e.g., display “lambda” as “λ”)
+;; Prettify Symbols (e.g., display "lambda" as "λ")
 (use-package prog-mode
   :ensure nil
   :hook (prog-mode . prettify-symbols-mode))
 
+;; ----------------------------------------------------------------------------
+;; Comint
+;; ----------------------------------------------------------------------------
 
-;;--------------------------------------------------------------------------
-;; Language-specific Modes
-;;--------------------------------------------------------------------------
-
-(use-package yaml-ts-mode)
+;; Scroll comint buffer (shell/REPL) to bottom on new output, but only in the
+;; active window.
+(setq comint-scroll-to-bottom-on-output 'this)
 
 (provide 'init-prog)
 ;;; init-prog.el ends here
