22d6278808b56891341c8e758ac815d30ee53ba3
Akane <710105188@qq.com>
2026-07-10 21:16:57 +0800

    feat(highlight): add hl-todo with bold keyword faces
    - Highlight TODO/FIXME/BUG only when followed by a colon, bold + Maple
      Mono NF CN on the keyword itself (not the colon).
    - Enable rainbow-delimiters only in lisp modes.

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -8,58 +8,25 @@
 
 ;;; Code:
 
-;;----------------------------------------------------------------------------
 ;; Current Line Highlighting
-;;----------------------------------------------------------------------------
-
 (use-package hl-line
   :hook prog-mode LaTeX-mode)
 
-;;----------------------------------------------------------------------------
 ;; Matching Parentheses Highlighting
-;;----------------------------------------------------------------------------
-
-;; `show-paren-mode` is a built-in mode that highlights matching delimiters
 (use-package paren
   :ensure nil
   :hook ((prog-mode text-mode) . show-paren-mode)
   :config
-  ;; These settings improve the highlighting behavior, making it more intuitive.
   (setq show-paren-when-point-inside-paren t
         show-paren-when-point-in-periphery t
         blink-matching-paren-highlight-offscreen t
         show-paren-context-when-offscreen 'overlay))
 
-
-;;----------------------------------------------------------------------------
-;; Symbol Occurrences Highlighting
-;;----------------------------------------------------------------------------
-
-;; `symbol-overlay` highlights all occurrences of the symbol at the current
-;; cursor position.
-;; (use-package symbol-overlay
-;;   :diminish
-;;   :hook (prog-mode . symbol-overlay-mode)
-;;   :bind (:map symbol-overlay-mode-map
-;;               ("M-i" . symbol-overlay-put)
-;;               ("M-n" . symbol-overlay-jump-next)
-;;               ("M-p" . symbol-overlay-jump-prev)))
-
-;;----------------------------------------------------------------------------
-;; Rainbow Delimiters
-;;----------------------------------------------------------------------------
-
-;; `rainbow-delimiters` assigns different colors to parentheses, brackets,
-;; and braces at different nesting levels.
-
+;; Rainbow Delimiters for lisp parentheses highlighting
 (use-package rainbow-delimiters
-  :hook (emacs-lisp-mode . rainbow-delimiters-mode))
+  :hook ((emacs-lisp-mode scheme-mode) . rainbow-delimiters-mode))
 
-
-;;----------------------------------------------------------------------------
 ;; VCS Changes Highlighting
-;;----------------------------------------------------------------------------
-
 (use-package diff-hl
   :autoload diff-hl-flydiff-mode
   :hook ((prog-mode . global-diff-hl-mode)
@@ -75,14 +42,12 @@
   (setq diff-hl-update-async t)
   (diff-hl-flydiff-mode 1))
 
-;;----------------------------------------------------------------------------
 ;; Doxygen Highlighting
-;;----------------------------------------------------------------------------
-
 (use-package highlight-doxygen
   :hook prog-mode
   :custom-face
   (highlight-doxygen-comment ((t (:background unspecified))))
+  (highlight-doxygen-variable ((t (:inherit eglot-semantic-parameter))))
   :config
   ;; Fix doxygen regex to match identifiers with underscores in C/C++ modes.
   (defun my/highlight-doxygen--patch-sw (tree)
@@ -100,6 +65,21 @@
   (define-advice highlight-doxygen-anchored-keywords-template
       (:filter-return (keywords) fix-underscore)
     (my/highlight-doxygen--patch-sw keywords)))
+
+;; Highlight TODO, FIXME, and BUG keywords in code comments
+(use-package hl-todo
+  :hook (prog-mode . hl-todo-mode)
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+        hl-todo-require-punctuation t
+        hl-todo-keyword-faces
+        '(("TODO"  . ((:weight bold :family "Maple Mono NF CN") font-lock-type-face))
+          ("FIXME" . ((:weight bold :family "Maple Mono NF CN") error))
+          ("BUG"   . ((:weight bold :family "Maple Mono NF CN") error))))
+  ;; Apply face to keyword only (group 2), not the trailing colon (group 1).
+  (setq hl-todo--keywords
+        `((,(lambda (bound) (hl-todo--search nil bound))
+           (2 (hl-todo--get-face) prepend t)))))
 
 (provide 'init-highlight)
 ;;; init-highlight.el ends here
