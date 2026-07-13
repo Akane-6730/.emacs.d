25252dc75a844d002fd338fad6b7188c4405b3cc
Akane <710105188@qq.com>
2026-07-13 18:44:10 +0800

    refactor(c): consolidate config into use-package

--- lisp/lang/init-c.el
+++ lisp/lang/init-c.el
@@ -8,137 +8,90 @@
 ;;
 ;;; Code:
 
-;;----------------------------------------------------------------------------
-;; Customizable Compilation Variables
-;;----------------------------------------------------------------------------
-(defvar my-c-common-flags "-Wall -Wextra" "Common compiler flags for C/C++.")
-(defvar my-c-std-version "" "The C standard to use (e.g., `c99`, `c11`).")
-(defvar my-cpp-std-version "" "The C++ standard to use (e.g., `c++14`, `c++17`).")
-(defvar my-c-debug-flags "-g" "Flags for including debug information.")
-(defconst my-c-type-keywords-regex "\\<\\(template\\|typename\\|namespace\\)\\>"
-  "Regex for C/C++ keywords styled as type-face.")
-(defvar-local my-c-font-lock-keywords-added nil
-  "Non-nil when custom C/C++ font-lock keywords are already installed.")
+(use-package c-ts-mode
+  :ensure nil
+  :preface
+  (defvar my-c-common-flags "-Wall -Wextra" "Common compiler flags for C/C++.")
+  (defvar my-c-std-version "" "The C standard to use (e.g., `c99`, `c11`).")
+  (defvar my-cpp-std-version "" "The C++ standard to use (e.g., `c++14`, `c++17`).")
+  (defvar my-c-debug-flags "-g" "Flags for including debug information.")
 
+  (defun my-c-match-symbol-in-code (regexp limit)
+    "Search for REGEXP up to LIMIT, skipping strings and comments."
+    (catch 'match
+      (while (re-search-forward regexp limit t)
+        (unless (nth 8 (syntax-ppss))
+          (throw 'match t)))))
 
-;;----------------------------------------------------------------------------
-;; The Unified C/C++ Tree-sitter Setup Function
-;;----------------------------------------------------------------------------
+  (defun my-c-ts-font-lock-matcher (limit)
+    "Match C/C++ delimiters and operators up to LIMIT using Tree-sitter."
+    (catch 'match
+      (while (re-search-forward
+              "\\(?:\\.\\|->\\|::\\|\\[\\|\\]\\|;\\|<\\|>\\|[\"']\\)" limit t)
+        (let* ((beg (match-beginning 0))
+               (end (match-end 0))
+               (text (match-string 0))
+               (node (treesit-node-at beg))
+               (type (and node (treesit-node-type node)))
+               (parent (and node (treesit-node-parent node))))
+          (when
+              (cond
+               ((equal type "escape_sequence") nil)
+               ((member text '("\"" "'"))
+                (and node
+                     (or (= (- (treesit-node-end node)
+                               (treesit-node-start node))
+                            1)
+                         (and (member type '("string_literal" "char_literal"
+                                             "system_lib_string"))
+                              (or (= beg (treesit-node-start node))
+                                  (= beg (1- (treesit-node-end node))))))))
+               ((member text '("<" ">"))
+                (or (and parent
+                         (member (treesit-node-type parent)
+                                 '("template_parameter_list"
+                                   "template_argument_list")))
+                    (equal type "system_lib_string")))
+               (node
+                (and (not (member type '("string_content" "comment")))
+                     (= (- (treesit-node-end node) (treesit-node-start node))
+                        (- end beg)))))
+            (throw 'match t))))))
 
-(defun my-c-ts-font-lock-matcher (limit)
-  "Match operators, templates, and quotes for custom highlighting.
-Uses Tree-sitter to ensure context correctness (e.g. excluding 3.14, string content)."
-  (let (res)
-    (while (and (not res)
-                (re-search-forward "\\(?:\\.\\|->\\|::\\|\\[\\|\\]\\|;\\|<\\|>\\|[\"']\\)" limit t))
-      (let* ((beg (match-beginning 0))
-             (end (match-end 0))
-             (node (treesit-node-at beg))
-             (parent (treesit-node-parent node))
-             (type (treesit-node-type node))
-             (text (match-string 0)))
-        (when node
-          (cond
-           ;; A. Escape sequences (\") -> SKIP
-           ((equal type "escape_sequence")
-            nil)
+  (defun my-c-ts-mode-setup ()
+    "Set up indentation, compilation, keys, and faces for C/C++."
+    (setq-local c-ts-mode-indent-style 'k&r
+                c-ts-indent-offset 4)
+    (setq-default c-basic-offset 4)
 
-           ;; B. Quotes (" ') -> Highlight only if delimiters
-           ((member text '("\"" "'"))
-            (when (or
-                   ;; Standalone delimiter node
-                   (= (- (treesit-node-end node) (treesit-node-start node)) 1)
-                   ;; Boundary of literal
-                   (and (member type '("string_literal" "char_literal" "system_lib_string"))
-                        (or (= beg (treesit-node-start node))
-                            (= beg (1- (treesit-node-end node))))))
-              (setq res t)))
+    (when buffer-file-name
+      (let* ((cpp-p (or (string-suffix-p ".cpp" buffer-file-name)
+                        (derived-mode-p 'c++-ts-mode)))
+             (compiler (if cpp-p "g++" "gcc"))
+             (standard (if cpp-p my-cpp-std-version my-c-std-version))
+             (file (file-name-nondirectory buffer-file-name)))
+        (setq-local compile-command
+                    (format "%s -o %s %s %s %s %s"
+                            compiler (file-name-sans-extension file) file
+                            my-c-common-flags my-c-debug-flags
+                            (if (string-empty-p standard)
+                                ""
+                              (concat "-std=" standard))))))
 
-           ;; C. Angle brackets (< >) -> Highlight if Template or Include
-           ((member text '("<" ">"))
-            (when (or
-                   ;; Template context
-                   (member (treesit-node-type parent)
-                           '("template_parameter_list" "template_argument_list"))
-                   ;; Include context (<time.h>)
-                   (member type '("system_lib_string")))
-              (setq res t)))
-
-           ;; D. Other operators (. -> :: ; [] etc) -> Highlight if operator
-           (t
-            (when (and
-                   ;; Not in string/comment content
-                   (not (member type '("string_content" "comment")))
-                   ;; Geometry match (excludes 3.14)
-                   (= (- (treesit-node-end node) (treesit-node-start node))
-                      (- end beg)))
-              (setq res t)))))))
-    res))
-
-(defun my-c-match-symbol-in-code (regex limit)
-  "Search for REGEX up to LIMIT, ensuring match is not in string or comment."
-  (let (res)
-    (while (and (not res)
-                (re-search-forward regex limit t))
-      (unless (nth 8 (syntax-ppss))
-        (setq res t)))
-    res))
-
-(defun my-c-ts-mode-setup ()
-  "Apply all custom settings for C/C++ Tree-sitter modes."
-  ;; 1. --- Indentation Style  ---
-  ;; This single variable controls indentation for both c-ts-mode and
-  ;; c++-ts-mode, as the latter inherits from the former.
-  (setq-local c-ts-mode-indent-style 'k&r)
-  (setq-local c-ts-indent-offset 4)
-  (setq-default c-basic-offset 4)
-
-  ;; 2. Setup Smart Compile Command
-  (when buffer-file-name
-    (let* ((is-cpp        (or (string-suffix-p ".cpp" buffer-file-name)
-                              (derived-mode-p 'c++-ts-mode)))
-           (compiler      (if is-cpp "g++" "gcc"))
-           (std-version   (if is-cpp my-cpp-std-version my-c-std-version))
-           (std-flag      (unless (string-empty-p std-version)
-                            (format "-std=%s" std-version)))
-           (file-name     (file-name-nondirectory buffer-file-name))
-           (file-sans-ext (file-name-sans-extension file-name)))
-      (setq-local compile-command
-                  (format "%s -o %s %s %s %s %s"
-                          compiler file-sans-ext file-name
-                          my-c-common-flags my-c-debug-flags (or std-flag "")))))
-
-  ;; 3. Setup Keybinding
-  (define-key (current-local-map) (kbd "<f12>") #'compile)
-
-  ;; 4. Custom Highlighting
-  (unless my-c-font-lock-keywords-added
     (font-lock-add-keywords
      nil
      `((my-c-ts-font-lock-matcher 0 'font-lock-delimiter-face t)
        ("^\\s-*\\(#\\)" 1 'font-lock-delimiter-face t)
-
-       ;; Keywords: template, typename, namespace -> Blue
-       (,(lambda (lim) (my-c-match-symbol-in-code my-c-type-keywords-regex lim))
+       (,(lambda (limit) (my-c-match-symbol-in-code "\\<namespace\\>" limit))
         0 'font-lock-type-face t)
-
-       ;; Keyword: this -> Orange
-       (,(lambda (lim) (my-c-match-symbol-in-code "\\<this\\>" lim))
-        0 'eglot-semantic-parameter t)))
-    (setq-local my-c-font-lock-keywords-added t)))
-
-;;----------------------------------------------------------------------------
-;; Hooking into Tree-sitter Modes
-;;----------------------------------------------------------------------------
-
-;; We explicitly add our setup function to the specific hooks for
-;; Tree-sitter modes. This is the most direct and reliable method.
-(add-hook 'c-ts-mode-hook #'my-c-ts-mode-setup)
-(add-hook 'c++-ts-mode-hook #'my-c-ts-mode-setup)
-
-;; Associate CUDA files with C++ modes
-(add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.cuh\\'" . c++-mode))
+       (,(lambda (limit) (my-c-match-symbol-in-code "\\<this\\>" limit))
+        0 'eglot-semantic-parameter t))))
+  :bind (:map c-ts-mode-map ("<f12>" . compile)
+              :map c++-ts-mode-map ("<f12>" . compile))
+  :mode (("\\.cu\\'" . c++-ts-mode)
+         ("\\.cuh\\'" . c++-mode))
+  :hook ((c-ts-mode . my-c-ts-mode-setup)
+         (c++-ts-mode . my-c-ts-mode-setup)))
 
 ;; Associate CMakeLists.txt with cmake-ts-mode
 (use-package cmake-ts-mode
